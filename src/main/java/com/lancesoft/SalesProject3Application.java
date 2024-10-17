04cf605eabb4660efa7fc128fcf062f4d60edede
Swathi Kovuru <2swathi000@gmail.com>
2024-10-17 02:20:02 -0400

    changed in main file

--- src/main/java/com/lancesoft/SalesProject3Application.java
+++ src/main/java/com/lancesoft/SalesProject3Application.java
@@ -9,9 +9,7 @@ public class SalesProject3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SalesProject3Application.class, args);
 	}
-<<<<<<< HEAD
+
 // where is my hahahahahahahahahah
-=======
 //hahahahahahahahahahahahaahahhahahaahahhahahaahahah
->>>>>>> 7078f902cccff72a2563531c44888a97ab6262d6
 }
