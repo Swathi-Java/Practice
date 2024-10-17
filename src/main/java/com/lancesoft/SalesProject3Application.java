7078f902cccff72a2563531c44888a97ab6262d6
Swathi-Java <120640493+Swathi-Java@users.noreply.github.com>
2024-10-17 01:49:08 -0400

    Update SalesProject3Application.java

--- src/main/java/com/lancesoft/SalesProject3Application.java
+++ src/main/java/com/lancesoft/SalesProject3Application.java
@@ -9,5 +9,5 @@ public class SalesProject3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SalesProject3Application.class, args);
 	}
-
+//hahahahahahahahahahahahaahahhahahaahahhahahaahahah
 }
